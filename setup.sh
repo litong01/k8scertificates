f43c42d85d6e11644c66271da1f177cb1445dde3
litong01 <litong01@us.ibm.com>
2021-02-05 22:10:19 +0000

    Changed to use local yaml files
    Signed-off-by: litong01 <litong01@us.ibm.com>

--- setup.sh
+++ setup.sh
@@ -4,17 +4,21 @@
 
 action=$1
 if [[ $action == 'delete' ]]; then
+
     echo "Removing mapping and ingress service for acme challenges..."
-    kubectl delete -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/mappingingress.yaml
+    # kubectl delete -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/mappingingress.yaml
+    kubectl delete -f yaml/mappingingress.yaml
     echo "Removing the certificate issuer..."
-    kubectl delete -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/issuer.yaml
+    # kubectl delete -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/issuer.yaml
+    kubectl delete -f yaml/issuer.yaml
 
     echo "Removing cert-manager..."
     kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 
     echo "Removing ambassador..."
-    kubectl delete -f https://www.getambassador.io/yaml/aes-crds.yaml && \
     kubectl delete -f https://www.getambassador.io/yaml/aes.yaml
+    kubectl delete -f https://www.getambassador.io/yaml/aes-crds.yaml && \
+
     echo "Ambassador and Cert-Manager are now removed."
 else
     echo "Installing ambassador..."
@@ -28,12 +32,17 @@ else
     echo "The external IP address: "$EXTERNALIP
     echo ""
     echo "Installing cert-manager..."
-    kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
+    kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml && \
+    kubectl -n cert-manager wait --for condition=available --timeout=90s deploy -lapp=webhook
 
     echo ""
     echo "Set up mapping and ingress service for acme challenges..."
-    kubectl apply -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/mappingingress.yaml
+    # kubectl apply -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/mappingingress.yaml
+    kubectl apply -f yaml/mappingingress.yaml
+    sleep 10
     echo "Set up the certificate issuer..."
-    kubectl apply -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/issuer.yaml
+    # kubectl apply -f https://raw.githubusercontent.com/litong01/k8scertificates/main/yaml/issuer.yaml
+    kubectl apply -f yaml/issuer.yaml
+
     echo "You are now ready to request a certificate"
 fi
